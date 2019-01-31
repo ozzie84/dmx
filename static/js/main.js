5576b790741746a153115f6837f6c6e347356c04
vitaliiyakymenko <git-1>
2019-01-31 15:55:42 +0200

    dmx8

--- static/js/main.js
+++ static/js/main.js
@@ -11,70 +11,13 @@
           sliderStart();
         }
       });
-    } // Tooltip
+    } // Search init
 
-
-    var tooltipTrigger = '.tooltip-trigger';
-    var inputTooltipTrigger = 'input' + tooltipTrigger;
-    var buttonTooltipTrigger = 'button' + tooltipTrigger; // Closes all tooltips on ESC when tooltip is triggered via focus,
-
-    $(tooltipTrigger).keydown(function (event) {
-      var instances = $.tooltipster.instances();
-
-      if (event.which === 27) {
-        // TODO: Figure out how to use randomly generated ID for tooltip, to close the selected tooltip instead of all of them.
-        $.each(instances, function (i, instance) {
-          instance.close();
-        });
-      }
-    }); // Initialization options for when inputs are tooltip triggers
-
-    $(inputTooltipTrigger).tooltipster({
-      trigger: 'custom'
-    }); // Show tooltip on focus of tooltip trigger
-
-    $(tooltipTrigger).focusin(function () {
-      $(this).tooltipster('show');
-    }); // Hide tooltip when focusing out of tooltip trigger
-
-    $(tooltipTrigger).focusout(function () {
-      $(this).tooltipster('hide');
-    }); // Show tooltip on hover when tooltip trigger is input
-
-    $(inputTooltipTrigger).hover(function () {
-      $(this).tooltipster('show');
-    }); // Prevents tooltip from closing on mouseleave when input is focused
-
-    $(inputTooltipTrigger).mouseleave(function () {
-      if (!$(this).is(":focus")) {
-        $(this).tooltipster('hide');
-      }
-    }); // Initialization options for when buttons are tooltip triggers
-
-    $(buttonTooltipTrigger).tooltipster({
-      trigger: 'custom',
-      theme: 'tooltipster-shadow',
-      triggerOpen: {
-        mouseenter: true,
-        touchstart: true,
-        click: false
-      },
-      triggerClose: {
-        click: true,
-        scroll: true,
-        tap: true,
-        mouseleave: false
-      }
-    }); // Fixing bug that causes focus on input fields not work properly
-
-    $(inputTooltipTrigger).on({
-      'touchstart': function touchstart() {
-        $(this).focus();
-      }
-    }); // Search init
 
     jsonInit();
-  });
+    jsonSearchInit();
+  }); // Modals init in Video Page
+
   $(document).on('click', '.video .banners_lg, .video .banners_sm__item', function () {
     var way = $(this).attr('data-video');
     var title = $(this).children().find('.banners__place').text();
@@ -331,14 +274,7 @@
   var swiperHistory = new Swiper('.about_history__slider .swiper-container', {
     speed: 1200,
     spaceBetween: 30,
-    loop: false,
-    parallax: true,
-    longSwipes: false,
-    breakpoints: {
-      576: {
-        parallax: false
-      }
-    }
+    loop: false
   });
   swiperHistory.on('transitionEnd', function () {
     var index = swiperHistory.activeIndex;
@@ -350,7 +286,7 @@
   });
   swiperHistory.on('slidePrevTransitionStart', function () {
     var index = swiperHistory.activeIndex;
-    controlActive.find('.active_point').removeClass('active_point');
+    controlActive.find('.active_point').removeClass('active_point').css('background', '#b4b4b4');
     setTimeout(function () {
       controlActive.children().eq(index).addClass('active_point');
     }, 800);
@@ -385,10 +321,14 @@
         setTimeout(function () {
           controlActive.children().eq(j).css('background', '#fec10e');
         }, 200);
+      } else {
+        setTimeout(function () {
+          controlActive.children().eq(j).css('background', '#b4b4b4');
+        }, 500);
       }
     };
 
-    for (var j = 0; j < $(this).index(); j++) {
+    for (var j = 0; j < $('.history_control span').length; j++) {
       _loop(j);
     }
 
@@ -473,7 +413,7 @@
     }
   }
 
-  $(window).bind('scroll', menuFixed); // Search
+  $(window).bind('scroll', menuFixed); // Search store
 
   function jsonInit() {
     var searchField = $('#search').val();
@@ -482,7 +422,8 @@
       $.each(data, function (key, value) {
         if (value.attributes == $('input[name="store"]:checked').val() || !$('input[name="store"]:checked').val()) {
           if (value.title.search(expression) !== -1 || value.location.search(expression) !== -1) {
-            $('#result').append("\n\t\t\t\t\t\t\t\t<li class=\"distributors_list__item\">\n\t\t\t\t\t\t\t\t\t<div class=\"distributors_list_img\">\n\t\t\t\t\t\t\t\t\t\t<img src=\"static/img/content/".concat(value.img, "\" alt=\"img\" class=\"distributors_list_img--this\">\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t\t<div class=\"distributors_list_content\">\n\t\t\t\t\t\t\t\t\t\t<h6 class=\"distributors_list__title\">").concat(value.title, "</h6>\n\t\t\t\t\t\t\t\t\t\t<p class=\"distributors_list__address\">").concat(value.location, "</p>\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t\t<div class=\"distributors_list_btn\">\n\t\t\t\t\t\t\t\t\t\t<button name=\"see\" class=\"btn_see\">\n\t\t\t\t\t\t\t\t\t\t\t<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" viewBox=\"0 0 511.999 511.999\" xml:space=\"preserve\">\n\t\t\t\t\t\t\t\t\t\t\t\t<g>\n\t\t\t\t\t\t\t\t\t\t\t\t\t<g>\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t<path d=\"M508.745,246.041c-4.574-6.257-113.557-153.206-252.748-153.206S7.818,239.784,3.249,246.035\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tc-4.332,5.936-4.332,13.987,0,19.923c4.569,6.257,113.557,153.206,252.748,153.206s248.174-146.95,252.748-153.201\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tC513.083,260.028,513.083,251.971,508.745,246.041z M255.997,385.406c-102.529,0-191.33-97.533-217.617-129.418\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tc26.253-31.913,114.868-129.395,217.617-129.395c102.524,0,191.319,97.516,217.617,129.418\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tC447.361,287.923,358.746,385.406,255.997,385.406z\"/>\n\t\t\t\t\t\t\t\t\t\t\t\t\t</g>\n\t\t\t\t\t\t\t\t\t\t\t\t</g>\n\t\t\t\t\t\t\t\t\t\t\t\t<g>\n\t\t\t\t\t\t\t\t\t\t\t\t\t<g>\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t<path d=\"M255.997,154.725c-55.842,0-101.275,45.433-101.275,101.275s45.433,101.275,101.275,101.275\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\ts101.275-45.433,101.275-101.275S311.839,154.725,255.997,154.725z M255.997,323.516c-37.23,0-67.516-30.287-67.516-67.516\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\ts30.287-67.516,67.516-67.516s67.516,30.287,67.516,67.516S293.227,323.516,255.997,323.516z\"/>\n\t\t\t\t\t\t\t\t\t\t\t\t\t</g>\n\t\t\t\t\t\t\t\t\t\t\t\t</g>\n\t\t\t\t\t\t\t\t\t\t\t\t</svg>\n\t\t\t\t\t\t\t\t\t\t</button>\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t</li>\n\t\t\t\t\t\t\t"));
+            $('#result').append("\n\t\t\t\t\t\t\t\t<li class=\"distributors_list__item\">\n\t\t\t\t\t\t\t\t\t<div class=\"distributors_list_img\">\n\t\t\t\t\t\t\t\t\t\t<img src=\"static/img/content/".concat(value.img, "\" alt=\"img\" class=\"distributors_list_img--this\">\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t\t<div class=\"distributors_list_content\">\n\t\t\t\t\t\t\t\t\t\t<h6 class=\"distributors_list__title\">").concat(value.title, "</h6>\n\t\t\t\t\t\t\t\t\t\t<p class=\"distributors_list__address\">").concat(value.location, "</p>\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t\t<div class=\"distributors_list_btn\">\n\t\t\t\t\t\t\t\t\t\t<button \n\t\t\t\t\t\t\t\t\t\t  name=\"see\"\n\t\t\t\t\t\t\t\t\t\t  data-id=\"").concat(value.unique, "\"\n\t\t\t\t\t\t\t\t\t\t  class=\"btn_see\"\n                    >\n\t\t\t\t\t\t\t\t\t\t\t<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" viewBox=\"0 0 511.999 511.999\" xml:space=\"preserve\">\n\t\t\t\t\t\t\t\t\t\t\t\t<g>\n\t\t\t\t\t\t\t\t\t\t\t\t\t<g>\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t<path d=\"M508.745,246.041c-4.574-6.257-113.557-153.206-252.748-153.206S7.818,239.784,3.249,246.035\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tc-4.332,5.936-4.332,13.987,0,19.923c4.569,6.257,113.557,153.206,252.748,153.206s248.174-146.95,252.748-153.201\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tC513.083,260.028,513.083,251.971,508.745,246.041z M255.997,385.406c-102.529,0-191.33-97.533-217.617-129.418\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tc26.253-31.913,114.868-129.395,217.617-129.395c102.524,0,191.319,97.516,217.617,129.418\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tC447.361,287.923,358.746,385.406,255.997,385.406z\"/>\n\t\t\t\t\t\t\t\t\t\t\t\t\t</g>\n\t\t\t\t\t\t\t\t\t\t\t\t</g>\n\t\t\t\t\t\t\t\t\t\t\t\t<g>\n\t\t\t\t\t\t\t\t\t\t\t\t\t<g>\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t<path d=\"M255.997,154.725c-55.842,0-101.275,45.433-101.275,101.275s45.433,101.275,101.275,101.275\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\ts101.275-45.433,101.275-101.275S311.839,154.725,255.997,154.725z M255.997,323.516c-37.23,0-67.516-30.287-67.516-67.516\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\ts30.287-67.516,67.516-67.516s67.516,30.287,67.516,67.516S293.227,323.516,255.997,323.516z\"/>\n\t\t\t\t\t\t\t\t\t\t\t\t\t</g>\n\t\t\t\t\t\t\t\t\t\t\t\t</g>\n\t\t\t\t\t\t\t\t\t\t\t\t</svg>\n\t\t\t\t\t\t\t\t\t\t</button>\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t</li>\n\t\t\t\t\t\t\t"));
+            $('.distributors_slide').append("\n            <button\n              name=\"country\"\n              data-tooltip-content=\"#tooltip_content\"\n              data-country=\"".concat(value.country, "\"\n              data-location=\"").concat(value.location, "\"\n              data-img=\"").concat(value.img, "\"\n              data-email=\"").concat(value.email, "\"\n              data-title=\"").concat(value.title, "\"\n              data-id=\"").concat(value.unique, "\"\n              class=\"tooltip-trigger country_dot\"\n              onmouseenter=\"tool_init($(this))\"\n              style=\"left:").concat(value.x, "; top: ").concat(value.y, "\"\n              >\n              <svg class=\"country_dot__svg\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" viewBox=\"0 0 512 512\" xml:space=\"preserve\">\n                <g>\n                  <g>\n                    <path d=\"M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035\n                      c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719\n                      c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z\"/>\n                  </g>\n                </g>\n                </svg>\n            </button>\n          "));
           }
         }
       });
@@ -490,11 +431,87 @@
   }
 
   $('input[name="store"]').change(function () {
-    $('#result').html('');
+    $('#result, .distributors_slide').html('');
     jsonInit();
   });
   $('#search').keyup(function () {
-    $('#result').html('');
+    $('#result, .distributors_slide').html('');
     jsonInit();
+  }); // Add parameters in toolpitster
+
+  $(document).on('click', 'button[name="country"]', function () {
+    $('.tooltip_content__email .distributors_list__address').text($(this).attr('data-email'));
+    $('.tooltip_content__location--country').text($(this).attr('data-country'));
+    $('.tooltip_content__location--this').text($(this).attr('data-location'));
+    $('.tooltip_content__info .global_midTitle').text($(this).attr('data-title'));
+    $('.tooltip_content__img--this').attr('src', 'static/img/content/' + $(this).attr('data-img'));
+  }); // Btn see tooltipster init
+
+  $(document).on('mouseenter', '.btn_see', function () {
+    var id = $(this).attr('data-id');
+    var see = $('.distributors_slide').find("[data-id='".concat(id, "']"))[0];
+    $(see).trigger('mouseenter');
   });
-})(jQuery);
+  $(document).on('click', '.btn_see', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('data-id');
+
+    if (!$(this).hasClass(id)) {
+      $(this).removeClass(id);
+      var see = $('.distributors_slide').find("[data-id='".concat(id, "']"))[0];
+      $(see).trigger('mouseenter');
+      $(see).trigger('click');
+    }
+  }); // Downloads search
+
+  function jsonSearchInit() {
+    var searchField = $('#downloads_search').val();
+    var expression = new RegExp(searchField, 'i');
+    $.getJSON('static/json/downloads.json', function (data) {
+      $.each(data, function (key, value) {
+        var show = false;
+        var html = "<div class=\"col-xl-8 col-lg-9\"> ";
+        $.each(value.items, function (k, v) {
+          if (v.name.search(expression) !== -1) {
+            show = true;
+            html += " <div class=\"downloads_contain\">\n              <div class=\"downloads_list_wrap\">\n                <ul class=\"downloads_list\">\n                  <li class=\"downloads_list__item\">\n                    <p class=\"global_text\">".concat(v.name, "</p>\n                  </li>\n                </ul>\n              </div>\n              <div class=\"downloads_btn_wrap\">\n                <div class=\"downloads_btn\">\n                  <div class=\"downloads_btn_wrapper\">\n                    <span class=\"downloads_btn__format\">").concat(v.format, "</span>\n                    <span class=\"downloads_btn__size\">").concat(v.size, "</span>\n                  </div>\n                  <div class=\"downloads_btn_low--btn\">\n                    <a href=\"").concat(v.href, "\" class=\"downloads_btn__this\">\n                      <svg version=\"1.1\" class=\"btn_downloads__svg\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" viewBox=\"0 0 471.2 471.2\" xml:space=\"preserve\">\n                        <g>\n                            <path d=\"M457.7,230.15c-7.5,0-13.5,6-13.5,13.5v122.8c0,33.4-27.2,60.5-60.5,60.5H87.5c-33.4,0-60.5-27.2-60.5-60.5v-124.8c0-7.5-6-13.5-13.5-13.5s-13.5,6-13.5,13.5v124.8c0,48.3,39.3,87.5,87.5,87.5h296.2c48.3,0,87.5-39.3,87.5-87.5v-122.8C471.2,236.25,465.2,230.15,457.7,230.15z\"/>\n                            <path d=\"M226.1,346.75c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4l85.8-85.8c5.3-5.3,5.3-13.8,0-19.1c-5.3-5.3-13.8-5.3-19.1,0l-62.7,62.8V30.75c0-7.5-6-13.5-13.5-13.5s-13.5,6-13.5,13.5v273.9l-62.8-62.8c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1L226.1,346.75z\"/>\n                        </g>\n                      </svg>\n                    </a>\n                  </div>\n                </div>\n              </div>\n            </div>");
+          }
+        });
+        html += " </div> ";
+        var prev = "<div class=\"row global_padding__row ".concat(show === false ? 'hidden-dw' : '', "\"><div class=\"col-xl-4 col-lg-3\"><h3 class=\"global_maxTitle\">").concat(value.title, "</h3></div>");
+        prev += html;
+        $('#downloads_result').append(prev);
+      });
+    });
+  }
+
+  $('#downloads_search').keyup(function () {
+    $('#downloads_result').html('');
+    jsonSearchInit();
+  });
+})(jQuery); // Tooltipster init in html
+
+
+function tool_init(elem) {
+  if (!elem.hasClass("tooltipstered")) {
+    elem.tooltipster({
+      theme: 'tooltipster-shadow',
+      contentCloning: true,
+      trigger: 'click',
+      animation: 'fade',
+      delay: 500,
+      contentAsHTML: true,
+      animationDuration: [450, 0],
+      functionReady: function functionReady() {
+        var id = elem.attr('data-id');
+        var see = $('.distributors_list').find("[data-id='".concat(id, "']")).addClass(id);
+        see.parents('.distributors_list__item').addClass('active_see');
+      },
+      functionAfter: function functionAfter() {
+        var id = elem.attr('data-id');
+        var see = $('.distributors_list').find("[data-id='".concat(id, "']")).removeClass(id);
+        see.parents('.distributors_list__item').removeClass('active_see');
+      }
+    });
+  }
+}
